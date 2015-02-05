41c035ab112e3ecb2778b8755ec75df07383d5f2
Kevin Keijzer <the-unconventional@users.noreply.github.com>
2015-02-05 10:02:00 +0100

    Create disable-lightdm-guest-session.sh

---
+++ disable-lightdm-guest-session.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Do a root check
+if [ $(id -u) -ne 0 ]; then echo "This script must be run as root.";exit 99;fi
+
+# Disable LightDM guest session
+mkdir /etc/lightdm/lightdm.conf.d/
+echo "[SeatDefaults]
+allow-guest=false" > /etc/lightdm/lightdm.conf.d/20-guest.conf
