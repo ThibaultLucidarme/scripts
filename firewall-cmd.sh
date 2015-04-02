0aaf35ce569753dbfe3a397e49ec0361db6bbadb
Kevin Keijzer <the_unconventional@fsfe.org>
2015-04-02 18:28:46 +0200

    Delete firewall-cmd.sh

--- firewall-cmd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo firewall-cmd --remove-service ssh --zone dmz --permanent
-sudo firewall-cmd --remove-service ssh --zone external --permanent
-sudo firewall-cmd --remove-service ssh --zone public --permanent
-sudo firewall-cmd --remove-service samba-client --zone home --permanent
-sudo firewall-cmd --remove-service samba-client --zone internal --permanent
-sudo firewall-cmd --reload
